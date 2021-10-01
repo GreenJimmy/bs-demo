c78fe8d7db47a8bef0723fe3608da3fee0daf56c
Yuliia Dementsova <ydementsova@transcenda.com>
2021-10-01 17:39:35 +0300

    feat: added ThemeProvider from styled-component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Button from './bs-components/Button';
 import CustomButton from './bs-components/CustomButton';
 
-const App = () => (
-    <>
-        <Button>hello world</Button>
-        <CustomButton variant="primary" />
-    </>
-);
+const App = () => {
+    const customTheme = {
+        '$btn-primary-bg': '#0070A8',
+        '$btn-primary-color': 'white',
+        '$btn-secondary-bg': '#D1D8E0',
+        '$btn-secondary-color': 'white',
+    };
+
+    return (
+        <>
+            <Button>hello world</Button>
+            <CustomButton variant="primary" theme={customTheme} />
+        </>
+    );
+};
 
 export default App;
--- src/bs-components/CustomButton/index.js
+++ src/bs-components/CustomButton/index.js
@@ -1,11 +1,37 @@
 import { Button } from 'react-bootstrap';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import PropTypes from 'prop-types';
+
 import './customButton.style.scss';
 
 const StyledButton = styled(Button)`
     font-weight: 700;
+    ${(props) => `
+        &.btn-primary {
+            background-color: ${props.theme['$btn-primary-bg']};
+            border-color: ${props.theme['$btn-primary-bg']};
+            color: ${props.theme['$btn-primary-color']};
+        };
+        &.btn-secondary {
+            background-color: ${props.theme['$btn-secondary-bg']};
+            border-color: ${props.theme['$btn-secondary-bg']};
+            color: ${props.theme['$btn-secondary-color']};
+        }
+    `};
 `;
 
-export default (props) => {
-    return <StyledButton {...props}>Primary</StyledButton>;
+const CustomButton = (props) => {
+    const { theme, ...accProps } = props;
+
+    return (
+        <ThemeProvider theme={theme}>
+            <StyledButton {...accProps}>Primary</StyledButton>
+        </ThemeProvider>
+    );
 };
+
+CustomButton.propTypes = {
+    theme: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export default CustomButton;
