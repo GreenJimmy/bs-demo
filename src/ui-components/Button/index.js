b487a0ce433b9374dffcf31012b3cb0c18ec34c9
Yuliia Dementsova <ydementsova@transcenda.com>
2021-10-05 17:37:14 +0300

    feat: added style and var for button component

--- src/ui-components/Button/index.js
+++ src/ui-components/Button/index.js
@@ -1,28 +1,30 @@
 import { Button } from 'react-bootstrap';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-import './button.styles.scss';
-
-const StyledButton = styled(Button)`
-    color: white;
-`;
+import './styles/index.scss';
 
 const CustomButton = (props) => {
-    const { children, ...accProps } = props;
+    const { children, className, ...accProps } = props;
+    const classNames = className ? `${className} с-btn` : 'с-btn';
 
-    return <StyledButton {...accProps}>{children}</StyledButton>;
+    return (
+        <Button className={classNames} {...accProps}>
+            {children}
+        </Button>
+    );
 };
 
 CustomButton.propTypes = {
+    className: PropTypes.string,
     disabled: PropTypes.bool,
-    variant: PropTypes.oneOf(['primary', 'secondary']),
+    variant: PropTypes.oneOf(['primary', 'secondary', 'link']),
     size: PropTypes.oneOf(['lg', 'sm']),
     onClick: PropTypes.func,
     children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
 };
 
 CustomButton.defaultProps = {
+    className: undefined,
     disabled: false,
     variant: 'primary',
     size: 'lg',
