9de5c7440deba544471bfc5c447d356f30442cae
JT <jathompson@mdsol.com>
2021-10-08 07:27:46 -0700

    Upgraded react-bootstrap

--- src/bs-components/Button/index.js
+++ src/bs-components/Button/index.js
@@ -6,14 +6,25 @@ import styled from 'styled-components';
 
 import buttonString from '../../../node_modules/bootstrap/scss/_buttons.scss?raw';
 
-console.log(buttonString);
+// console.log(buttonString);
 
 const StyledButton = styled('div')``;
 
 export default ({ children, ...props }) => {
     const ast = parse(buttonString);
     const $ = createQueryWrapper(ast);
-    console.log($('rule'));
+    // console.log(ast);
+    console.log($('class'));
+    ast.value.map((value) => {
+        if (value.type === 'rule') {
+            value.value.map((subvalue) => {
+                if (subvalue.type === 'selector') {
+                    console.log(subvalue);
+                }
+            });
+        }
+        return null;
+    });
     return (
         <StyledButton>
             <Button {...props}>{children}</Button>
