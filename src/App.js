69e22a9bbad1ff1f9d8a35b863491a1b0e150849
Yuliia Dementsova <ydementsova@transcenda.com>
2021-10-12 19:25:20 +0300

    example of conflict

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import Dropdown from './ui-components/Dropdown';
+import { useState } from 'react';
 import Button from './bs-components/Button';
+import CustomButton from './ui-components/Button';
+import Check from './ui-components/Check';
 
 const App = () => {
-    const dropdownItems = [
-        { title: 'Action', eventKey: '1' },
-        { title: 'Another action', eventKey: '2', disabled: true, hasDividerAfter: true },
-        { title: 'Active Item', eventKey: '3' },
-    ];
+    const [checked, setCheck] = useState(false);
+
+    const onHandleChange = (event) => {
+        setCheck(event.target.checked);
+    };
+
     return (
         <div style={{ padding: '50px' }}>
             <Button>Clickable</Button>
             <br />
-            <Dropdown
-                id="custom-dropdown-id"
-                variant="primary"
-                items={dropdownItems}
-                className="my-custom-dropdown"
-                title="Dropdown button"
-            />
+            <div>UI components</div>
+            <div>
+                <Check
+                    id="custom-check-component"
+                    label="check box"
+                    checked={checked}
+                    onChange={onHandleChange}
+                />
+            </div>
+            <div>
+                <CustomButton>Button example</CustomButton>
+            </div>
         </div>
     );
 };
