cfd8010b3b06a362c4bee11c37cc7e2c30864237
ekabachenko <ekabachenko@transcenda.com>
2021-11-17 14:51:30 +0200

    fixed border styles

--- src/ui-components/Avatar/utils/index.js
+++ src/ui-components/Avatar/utils/index.js
@@ -1,11 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const getInitials = (name) => {
     const names = name.trim().split(/\s/);
-    return names.reduce((acc, curr, index) => {
-        if (index === 0 || index === names.length - 1) {
-            // eslint-disable-next-line no-param-reassign
-            acc = `${acc}${curr.charAt(0).toUpperCase()}`;
-        }
-        return acc;
-    }, '');
+    return names.length
+        ? `${names[0]?.charAt(0)}${names[names.length - 1]?.charAt(0)}`.toUpperCase()
+        : '';
 };
