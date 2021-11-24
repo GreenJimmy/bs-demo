70793c63fb21cfd9c1eb5881a5bb830de974c036
Yuliia Dementsova <ydementsova@transcenda.com>
2021-11-24 12:54:10 +0200

    feat: added table component example

--- src/view-components/index.js
+++ src/view-components/index.js
@@ -19,6 +19,14 @@ import {
     Select,
     InlineEditText,
     InlineEditSelect,
+    TableControl,
+    TableName,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    DataTableCell,
+    Table,
 } from '../ui-components';
 
 import {
@@ -41,10 +49,11 @@ import {
     selectData,
     selectOptions,
     inlineEditTextData,
-    inlineEditSelectData
+    inlineEditSelectData,
 } from './data';
 
 import './styles.scss';
+import ExclamationFillIcon from '../icons/ExclamationFillIcon';
 
 const ViewComponentsData = [
     { title: 'Avatar', component: (props) => <Avatar {...props} />, data: avatarData },
@@ -126,6 +135,84 @@ const ViewComponents = () => {
                         ))}
                     </ToggleButtonGroup>
                 ))}
+            </div>
+            <div className="header">Table</div>
+            <div className="component-block">
+                <Table
+                    tableStyle="filled"
+                    hover
+                    tableControlComponent={
+                        <TableControl>
+                            <TableName badgeNumber="100">Test table</TableName>
+                            <TextField controlId="search" placeholder="search" showDefaultIcon />
+                        </TableControl>
+                    }
+                >
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center" width="60px">
+                                <Check id="header-check" size="sm" />
+                            </TableCell>
+                            <TableCell width="50%">Study name</TableCell>
+                            <TableCell>Study short name</TableCell>
+                            <TableCell>Phase</TableCell>
+                            <TableCell align="center" width="60px">
+                                <ExclamationFillIcon />
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell align="center">
+                                <Check id="check-1" size="sm" />
+                            </TableCell>
+                            <TableCell>
+                                <Avatar
+                                    src="https://robohash.org/my-own-slug.png?size=300x300&set=set1"
+                                    size="md"
+                                />
+                                Clinical and Radiological Evaluation of Patients With
+                                Vertebrobasilar Insufficiency
+                            </TableCell>
+                            <TableCell>Sinus pressure study</TableCell>
+                            <TableCell>
+                                <Badge size="sm" type="default">
+                                    19
+                                </Badge>
+                            </TableCell>
+                            <TableCell align="center">
+                                <ExclamationFillIcon />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell align="center">
+                                <Check id="check-2" size="sm" />
+                            </TableCell>
+                            <TableCell>
+                                <DataTableCell
+                                    img={
+                                        <Avatar
+                                            src="https://robohash.org/my-own-slug.png?size=300x300&set=set1"
+                                            size="md"
+                                        />
+                                    }
+                                    subTitle="Vertebrobasilar Insufficiency"
+                                >
+                                    Clinical and Radiological Evaluation of Patients With
+                                </DataTableCell>
+                            </TableCell>
+                            <TableCell>Sinus pressure study</TableCell>
+                            <TableCell>
+                                <Badge size="sm" type="default">
+                                    8
+                                </Badge>
+                            </TableCell>
+                            <TableCell align="center">
+                                <ExclamationFillIcon />
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
             </div>
         </div>
     );
